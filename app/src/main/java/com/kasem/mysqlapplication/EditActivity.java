d8783b20e75a56dc245ab11852336edac000bc58
Kasemsuk Laiwtakul <kasame_suk@otmail.com>
2017-03-16 10:57:29 +0700

    fix red layout

--- app/src/main/java/com/kasem/mysqlapplication/EditActivity.java
+++ app/src/main/java/com/kasem/mysqlapplication/EditActivity.java
@@ -11,7 +11,7 @@ public class EditActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.id.layout.activity_edit);
+        setContentView(R.layout.activity_edit);
 
         final TodoList editTodoList = (TodoList) getIntent().getSerializableExtra("editTodoList");
 
